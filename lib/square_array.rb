45baed78dc0284a0a90ee441d2e61251d48e3981
Daniel Hahm <dhahm01@gmail.com>
2020-06-08 17:41:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   count = 0 
   new_array = []
   while count < times do 
-    new_array.push(Math.pow(array[count],2))
+    new_array.push(array[count] **2)
     count += 1
   end
   return new_array
